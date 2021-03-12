22f285b6c46bcfda265f271b5e71380f44997383
Jack Cross <jmc8252@psu.edu>
2021-03-12 12:13:06 -0500

    bar button reference in UI file

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -16,7 +16,16 @@ class TweetViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func cancel(_ sender: Any) {
+    }
+    
+    
+    @IBAction func tweet(_ sender: Any) {
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
